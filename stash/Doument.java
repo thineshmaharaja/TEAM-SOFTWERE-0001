d9805343e333950d140a701e6202e19321b168ef
thineshmaharaja <thineshthor174@gmail.com>
2020-09-21 11:25:39 +0530

    java file changes 2

---
+++ stash/Doument.java
@@ -0,0 +1,65 @@
+public class Document
+{
+   private String name;
+   private String extension;
+   private String contect;
+   private int id;
+   
+
+       public void setName(String name)
+       {
+             this.name=name;
+       } 
+       public void setExtension(String extension)
+       {
+	       this.Extension=Extension;
+       } 
+       public void setContent(Srting content)
+       {     
+               this.Content=Content;
+       }  
+       public void setId(int id)
+       {          
+               this.Id=Id;
+       } 
+
+
+
+       public String getName()
+       {   
+             return Name;
+       }
+       public String getExtension()
+       {     
+             return Extension;
+       }
+       public String getContent()
+       {  
+             return Content;
+       }
+       public int getId()
+       {  
+             return Id;
+       }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
